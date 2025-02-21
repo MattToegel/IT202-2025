ab5870493610440cc01e475f0294d3d4758aa05e
MattToegel <matthew.toegel@njit.edu>
2025-02-21 17:00:35 -0500

    updated wording for problem4 challenge 4, for future attempts

--- public_html/M2/problem4.php
+++ public_html/M2/problem4.php
@@ -20,9 +20,9 @@ function transformText($arr, $arrayNumber) {
     // Challenge 2: Convert text to Title Case
     // Challenge 3: Trim leading/trailing spaces and remove duplicate spaces
     // Result 1-3: Assign final phrase to `$placeholderForModifiedPhrase`
-    // Challenge 4 (extra credit): Extract middle 3 characters (beginning starts at middle of phrase),
+    // Challenge 4 (extra credit): Extract middle 3 characters (middle index and +/- 1 if it's not the first/last character),
     // assign to `$placeholderForMiddleCharacters`
-    // if not enough characters assign "Not enough characters"
+    // If the phrase is shorter than 3 characters, return "Not enough characters"
 
     // Step 1: sketch out plan using comments (include ucid and date)
     // Step 2: Add/commit your outline of comments (required for full credit)
