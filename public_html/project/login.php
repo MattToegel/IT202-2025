37532d60e122d8592c30ece9999edaeda92e4330
Matt Toegel <matthew.toegel@njit.edu>
2025-03-05 13:36:31 -0500

    Update login.php
    Fixing the dumb

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -55,7 +55,7 @@ if (
     if (!$hasError) {
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, password from `S25-010-Users` where email = :email");
+        $stmt = $db->prepare("SELECT id, email, password from `Users` where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
