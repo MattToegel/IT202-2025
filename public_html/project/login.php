30ebf1a2a140de51b42e28b7ae6f30ed1cbf73f6
MattToegel <matthew.toegel@njit.edu>
2025-06-14 23:22:48 -0400

    wip

--- public_html/project/login.php
+++ public_html/project/login.php
@@ -10,11 +10,7 @@ require(__DIR__."/../../partials/nav.php");
         <label for="pw">Password</label>
         <input type="password" id="pw" name="password" required minlength="8" />
     </div>
-    <div>
-        <label for="confirm">Confirm</label>
-        <input type="password" name="confirm" required minlength="8" />
-    </div>
-    <input type="submit" value="Register" />
+    <input type="submit" value="Login" />
 </form>
 <script>
     function validate(form) {
@@ -26,11 +22,10 @@ require(__DIR__."/../../partials/nav.php");
 </script>
 <?php
  //TODO 2: add PHP Code
- if (isset($_POST["email"], $_POST["password"], $_POST["confirm"])) {
+ if (isset($_POST["email"], $_POST["password"])) {
 
     $email = se($_POST, "email", "", false); 
     $password = se($_POST, "password", "", false);
-    $confirm = se($_POST, "confirm", "", false);
     // TODO 3: validate/use
     $hasError = false;
 
@@ -41,16 +36,11 @@ require(__DIR__."/../../partials/nav.php");
     // Sanitize and validate email
     $email = filter_var($email, FILTER_SANITIZE_EMAIL);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        echo "Invalid email address<br>";
+        echo "Invalid email address";
         $hasError = true;
     }
     if (empty($password)) {
         echo "Password must not be empty<br>";
-        $hasError = true;
-    }
-
-    if (empty($confirm)) {
-        echo "Confirm password must not be empty<br>";
         $hasError = true;
     }
 
@@ -59,25 +49,10 @@ require(__DIR__."/../../partials/nav.php");
         $hasError = true;
     }
 
-    if ($password !== $confirm) {
-        echo "Passwords must match<br>";
-        $hasError = true;
-    }
-
     if (!$hasError) {
-        // TODO 4: Hash password and store record in DB
-        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-        $db = getDB(); // available due to the `require()` of `functions.php`
-        // Code for inserting user data into the database
-        $stmt = $db->prepare("INSERT INTO Users (email, password) VALUES (:email, :password)");
-        try{
-            $stmt->execute([':email' => $email, ':password' => $hashed_password]);
-            echo "Successfully registered!<br>";
-        }
-        catch(Exception $e){
-            echo "There was an error registering<br>"; // user-friendly message
-            error_log("Registration Error: " . var_export($e, true)); // log the technical error for debugging
-        }
+        
+        // TODO 4: Check password and fetch user
+        
     }
 }
 ?>
