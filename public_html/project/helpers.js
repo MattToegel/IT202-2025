f14bf53ddacac1a4d6294f1b5ba4d879e83f44dc
MattToegel <matthew.toegel@njit.edu>
2025-07-19 22:24:49 -0400

    cleanup post merge

--- public_html/project/helpers.js
+++ public_html/project/helpers.js
@@ -1,4 +1,4 @@
-function flash (message = "", color = "info") {
+function flash(message = "", color = "info") {
     let flash = document.getElementById("flash");
     //create a div (or whatever wrapper we want)
     let outerDiv = document.createElement("div");
@@ -8,9 +8,13 @@ function flash (message = "", color = "info") {
     //apply the CSS (these are bootstrap classes which we'll learn later)
     innerDiv.className = `alert alert-${color}`;
     //set the content
-    innerDiv.innerText = message;
+    innerDiv.innerText = `(js) ${message}`;
 
     outerDiv.appendChild(innerDiv);
     //add the element to the DOM (if we don't it merely exists in memory)
     flash.appendChild(outerDiv);
+}
+
+function isValidPassword(val) {
+    return val && val.length >= 8;
 }
--- public_html/project/profile.php
+++ public_html/project/profile.php
@@ -185,21 +185,7 @@ if (isset($_POST["currentPassword"], $_POST["newPassword"], $_POST["confirmPassw
         //find the flash container, create a new element, appendChild
         // NOTE: we'll extract the flash code to a function later
         if (pw !== con) { // first JS validation example
-            //find the container
-            let flash = document.getElementById("flash");
-            //create a div (or whatever wrapper we want)
-            let outerDiv = document.createElement("div");
-            outerDiv.className = "row justify-content-center";
-            let innerDiv = document.createElement("div");
-
-            //apply the CSS (these are bootstrap classes which we'll learn later)
-            innerDiv.className = "alert alert-warning";
-            //set the content
-            innerDiv.innerText = "Password and Confirm password must match";
-
-            outerDiv.appendChild(innerDiv);
-            //add the element to the DOM (if we don't it merely exists in memory)
-            flash.appendChild(outerDiv);
+            flash("Password and Confirm password must match");
             isValid = false;
         }
         // returning false will prevent the form from submitting
